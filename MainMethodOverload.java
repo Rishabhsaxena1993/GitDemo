5d39bafa53f8a30ed14f69b831b83789a39ee2ee
= <rishabhsaxena@hotmail.com>
2021-03-04 19:16:38 +0530

    First committ

---
+++ MainMethodOverload.java
@@ -0,0 +1,16 @@
+package Interview_Practice;
+
+public class MainMethodOverload {
+
+	public static void main (String [] args) {
+
+		System.out.println("Integer");
+	}	
+	public static void main (char  args)  {
+			System.out.println("char");
+	}		
+	public static void main (Integer  args) {
+			System.out.println("original");
+		}
+			
+		}
